0a2efa19c7a3480c9aa5bd574daeac8a242c371e
C-Hurtmann <constantine2903@gmail.com>
2023-08-17 21:58:12 +0300

    add create date to Vote model

--- inforce/employee/models.py
+++ inforce/employee/models.py
@@ -5,5 +5,11 @@ from restaurant.models import Menu
 
 # Create your models here.
 class Vote(models.Model):
-    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     menu = models.ForeignKey(Menu, on_delete=models.CASCADE)
+    create_date = models.DateTimeField(auto_now_add=True)
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(fields=['create_date', 'user'], name='Vote of user on date')
+        ]
