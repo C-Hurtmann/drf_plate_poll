a8975f24e7bac2f63c2bdcdb5b269f0bd89d5ae2
C-Hurtmann <constantine2903@gmail.com>
2023-08-17 21:33:55 +0300

    create result method

--- inforce/employee/views.py
+++ inforce/employee/views.py
@@ -1,11 +1,25 @@
+from django.db.models import Count
 from rest_framework import viewsets
+from rest_framework.decorators import action
+from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
 
 from .models import Vote
 from .serializers import VoteSerializer
-
+from .permissions import IsEmployee
 # Create your views here.
+
 class VoteViewSet(viewsets.ModelViewSet):
     queryset = Vote.objects.all()
     serializer_class = VoteSerializer
-    permission_classes = (IsAuthenticated,)
+    permission_classes = (IsAuthenticated, IsEmployee)
+    
+    @action(detail=False, methods=['GET'])
+    def result(self, request):
+        vote_results = self.queryset.values('menu').annotate(count=Count('menu')).order_by('-count')
+        winner = max(vote_results, key=lambda x: x['count'])
+        response_data = {
+            'winner': winner if winner else None,
+            'results': vote_results
+        }
+        return Response(response_data)
