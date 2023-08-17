a49239607dd04aa28474cade44e8325a1eb3f495
C-Hurtmann <constantine2903@gmail.com>
2023-08-17 20:26:17 +0300

    change imports

--- inforce/restaurant/views.py
+++ inforce/restaurant/views.py
@@ -1,8 +1,8 @@
 from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated
 
-from .models import *
-from .serializers import *
+from .models import Restaurant, Dish, Menu
+from .serializers import RestaurantSerializer, DishSerializer, MenuSerializer
 from .permissions import IsRestauratorOrReadOnly
 # Create your views here.
 
